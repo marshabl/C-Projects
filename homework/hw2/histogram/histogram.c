6d38368627c974a14df9e71b2c5600ded735efb3
Blair Lewis Marshall <marshabl12@linux3.cs.uchicago.edu>
2019-10-19 10:59:01 -0500

    Update c file with better comments[

--- homework/hw2/histogram/histogram.c
+++ homework/hw2/histogram/histogram.c
@@ -4,30 +4,40 @@
 
 int main(int argc, char ** args)
 {
+	//create array of length 10 for digits
 	int digits[10] = {0};
+	
+	//create array of length 26 for letters
 	int letters[26] = {0};
+
+	//i, j for 'for' loops
 	int i,j;
 
+	//open file given via command line
 	FILE *fi = fopen(args[1], "r");
 
+	//read in one character at a time from file
 	char c;
 	while ((c=fgetc(fi))!=EOF)
 	{
+		//change upper to lower and count lower
 		if (c >= 'A' && c<= 'Z')
 		{
 			++letters[tolower(c)-'a'];
 		}
+		//count digits
 		if (c >= '0' && c<= '9')
 		{
 			++digits[c-'0'];
 		}
-		
+		//count lowers
 		if (c >= 'a' && c<= 'z')
 		{
 			++letters[c-'a'];
 		}			
 	}
 	
+	//print histogram of digits
         for (i = 0; i < 10; ++i)
 	{
 		if(digits[i] > 0)
@@ -40,7 +50,8 @@ int main(int argc, char ** args)
 			printf(" (%d)\n", digits[i]); 
 		}
 	}
-
+	
+	//print histogram of letters
 	for (i = 0; i < 26; ++i)
         {
                 if(letters[i] > 0)
