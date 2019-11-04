2658ed55a881e1a38f7d5fa1ceb2aed8b54b2f93
Blair Lewis Marshall <marshabl12@linux1.cs.uchicago.edu>
2019-11-03 22:44:48 -0600

    Update sizeof functions.

--- homework/hw4/cstring/mymemory.c
+++ homework/hw4/cstring/mymemory.c
@@ -6,7 +6,7 @@
 char * mystrdup(const char * mystr)
 {
 	unsigned int len = strlen(mystr); // determine length of the string
-	char* ptr = malloc((len+1)*sizeof(char*)); // alloc mem to hold a copy incl. null term
+	char* ptr = malloc((len+1)*sizeof(char)); // alloc mem to hold a copy incl. null term
 	strcpy(ptr, mystr); // copy string to new alloc memory
 	return ptr;
 }
@@ -14,7 +14,7 @@ char * mystrdup(const char * mystr)
 char ** add_word(char ** array, unsigned int wordcount, const char * word_to_add)
 {
 	char * copyword = mystrdup(word_to_add); // create copy of word to add
-	char **newarray = realloc(array, sizeof(char*)*(wordcount+1)); // add one to size of new array
+	char **newarray = realloc(array, sizeof(char)*(wordcount+1)); // add one to size of new array
 	newarray[wordcount] = copyword; // copy word to new array
 	return newarray;
 }
