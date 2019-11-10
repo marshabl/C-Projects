18e302036e597fa7a8b8583ddda0803893430076
Blair Lewis Marshall <marshabl12@linux1.cs.uchicago.edu>
2019-11-10 12:23:07 -0600

    mnist cfile

---
+++ homework/hw5/mnist/mnist.c
@@ -0,0 +1,315 @@
+#include "mnist.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <stdint.h>
+
+struct mnist_dataset_t
+{
+	unsigned int width;
+	unsigned int height;
+	struct mnist_image_t * start;
+	unsigned int imagecount;
+};
+
+struct mnist_image_t
+{
+	unsigned char * imagedata;
+        int label;
+        struct mnist_image_t * next_image;
+	struct mnist_image_t * prev_image;
+};
+
+mnist_dataset_handle mnist_create(unsigned int x, unsigned int y)
+{
+	struct mnist_dataset_t mnist_dataset_t;
+	mnist_dataset_handle h = malloc(sizeof(mnist_dataset_t));
+
+	//if malloc fails to find space
+	if (!h)
+	{
+		return MNIST_DATASET_INVALID;
+	}
+
+	h->width = x;	
+	h->height = y;
+	h->start = NULL; //no imagedata initially so pointing to null, must be freed
+	h->imagecount = 0;
+	
+	return h;
+}
+
+void mnist_free (mnist_dataset_handle handle)
+{
+	if (handle->start != NULL)
+	{
+		struct mnist_image_t mnist_image_t;
+		mnist_image_handle imh = handle->start;
+		mnist_image_handle next = NULL;
+		
+		for (unsigned int i=0; i<handle->imagecount; ++i)
+		{
+			next = imh->next_image;
+			free(imh->imagedata);
+			free(imh);
+			imh = next;
+		}
+	}
+
+	free(handle);
+}
+
+int mnist_image_count (const_mnist_dataset_handle handle)
+{
+	//if handle not valid, return negative imagecounts
+	if (handle == MNIST_DATASET_INVALID)
+	{
+		return -1;
+	}
+	return handle->imagecount;
+}
+
+void mnist_image_size (const_mnist_dataset_handle handle, unsigned int * x, unsigned int * y)
+{	
+	if (handle == MNIST_DATASET_INVALID)
+	{
+		*x = 0;
+        	*y = 0;
+	}
+	else
+	{
+		*x = handle->width;
+        	*y = handle->height;
+	}
+}
+
+mnist_dataset_handle mnist_open (const char * name)
+{
+	char dsfilename[255];
+	char lsfilename[255];
+	snprintf(dsfilename, sizeof(dsfilename), "%s-images-idx3-ubyte", name);
+	snprintf(lsfilename, sizeof(lsfilename), "%s-labels-idx1-ubyte", name);
+	
+	//initialize standard width variables
+	uint32_t magic_number;
+        uint32_t num_of_items;
+	uint32_t num_of_labels;
+	uint32_t width;
+	uint32_t height;
+	
+	//initialize pointers
+        char * mnptr = (char*)&magic_number;
+        char * itemptr = (char*)&num_of_items;
+	char * numlblptr = (char*)&num_of_labels;
+	char * wdthptr = (char*)&width;
+	char * hgtptr = (char*)&height;
+
+	//open files
+	FILE * dataset = fopen(dsfilename, "rb");
+	FILE * labelset = fopen(lsfilename, "rb");
+
+	//if open files fails, print error, exit program
+	if (!dataset || !labelset)
+        {
+                fprintf (stderr, "error: file open failed '%s'.\n", name);
+                exit(1);
+        }
+
+	/********************************DATASET***********************************/
+	//read in magic number for dataset and check if its correct
+	fread(mnptr, sizeof(unsigned char), sizeof(magic_number), dataset);
+        magic_number = ntohl(magic_number);
+
+        if (magic_number != 2051)
+        {
+                fprintf(stderr, "error: magic number does not equal 2049.");
+		exit(1);
+        }
+
+	//read in num items check correct
+        fread(itemptr, sizeof(unsigned char), sizeof(num_of_items), dataset);
+        num_of_items = ntohl(num_of_items);
+
+	//read in width and height
+        fread(wdthptr, sizeof(unsigned char), sizeof(width), dataset);
+        fread(hgtptr, sizeof(unsigned char), sizeof(height), dataset);
+        width = ntohl(width);
+        height = ntohl(height);
+	
+	/********************************LABELS***********************************/
+	//read in magic number for labelset and check if its correct
+	fread(mnptr, sizeof(unsigned char), sizeof(magic_number), labelset);
+        magic_number = ntohl(magic_number);
+
+        if (magic_number != 2049)
+        {
+                fprintf(stderr, "error: magic number does not equal 2049.");
+		exit(1);
+        }
+
+	fread(numlblptr, sizeof(unsigned char), sizeof(num_of_labels), labelset);
+        num_of_labels = ntohl(num_of_labels);
+
+	/****************************READ IN DATA + LABELS*******************************/
+	//initialize new empty dataset with above height and width
+	mnist_dataset_handle h =  mnist_create(width, height);	
+	h->imagecount = num_of_items;
+	
+	//set start parameters
+	mnist_image_handle imh = mnist_image_begin(h);
+	const unsigned char * imagedata;
+	unsigned int newlabel;
+	//read in data
+        for (int i=0; i < num_of_items; ++i)
+        {		
+		imh = mnist_image_add_after(h, imh, imagedata, width, height); 
+		fread(imh->imagedata, sizeof(unsigned char), height*width, dataset);
+		newlabel = mnist_image_label(imh);
+		newlabel = 0;
+                fread(&newlabel, sizeof(unsigned char), 1, labelset);	
+		mnist_set_image_label(imh, newlabel);
+        }
+
+	fclose(dataset);
+	fclose(labelset);
+	return h;
+}
+
+mnist_image_handle mnist_image_begin (mnist_dataset_handle handle)
+{
+	if (!handle || handle==MNIST_DATASET_INVALID)
+	{
+		return MNIST_IMAGE_INVALID;
+	}
+
+	return handle->start;
+}
+
+const unsigned char * mnist_image_data (const_mnist_image_handle h)
+{
+	if (!h || h==MNIST_IMAGE_INVALID)
+	{
+                fprintf(stderr, "error: no image data");
+		exit(1);
+	}
+	return h->imagedata;
+}
+
+int mnist_image_label (const_mnist_image_handle h)
+{
+	if (!h)
+        {
+                return -1;
+        }
+
+	return h->label;
+}
+
+int mnist_set_image_label(mnist_image_handle h, unsigned int newlabel)
+{
+	if (!h)
+        {
+                return -1;
+        }
+
+	h->label = newlabel;
+	return newlabel;
+}
+
+mnist_image_handle mnist_image_next (mnist_image_handle h)
+{
+	if (!h)
+        {
+                return MNIST_IMAGE_INVALID;
+        }
+	
+	return h->next_image;
+}
+
+mnist_image_handle mnist_image_prev (mnist_image_handle h)
+{
+	if (!h || h->prev_image == NULL)
+	{
+		return MNIST_IMAGE_INVALID;
+	}
+	
+	return h->prev_image;
+}
+
+mnist_image_handle mnist_image_add_after (mnist_dataset_handle dataset, mnist_image_handle h, const unsigned char * imagedata, unsigned int x, unsigned int y)
+{
+	if(!dataset || dataset->width!=x || dataset->height!=y)
+	{
+		return MNIST_IMAGE_INVALID;
+	}
+
+	struct mnist_image_t mnist_image_t;
+        mnist_image_handle imh = malloc(sizeof(mnist_image_t));
+	imh->imagedata = malloc((x * y * sizeof(unsigned char)));
+	imh->label = -1;
+	imh->next_image = NULL;
+
+	if (!h || h == MNIST_IMAGE_INVALID)
+	{
+		imh->prev_image = NULL;
+		dataset->start = imh;
+		
+	}
+	else
+	{
+		imh->prev_image = h;
+		h->next_image = imh;
+	}
+
+	return imh;
+}
+
+mnist_image_handle mnist_image_remove(mnist_dataset_handle dataset, mnist_image_handle h)
+{
+	if (!dataset || dataset == MNIST_DATASET_INVALID || !h || h == MNIST_IMAGE_INVALID)
+	{
+		return MNIST_IMAGE_INVALID;
+	}
+
+	mnist_image_handle first = mnist_image_begin(dataset);
+	if (h == first)
+	{
+		dataset->start = first->next_image;
+		first->next_image->prev_image = NULL;
+		mnist_image_handle second = first->next_image;
+		--dataset->imagecount;
+		free(first->imagedata);
+		free(first);
+		return second;
+	}
+	else
+	{
+		mnist_image_handle current = first;
+		for(int i =0; i<dataset->imagecount-1; ++i)
+		{
+			//prev = mnist_image_prev(current);
+			current = mnist_image_next(current);
+                	if (h == current)
+                	{
+                        	current->prev_image->next_image = current->next_image;
+				current->next_image->prev_image = current->prev_image;
+				mnist_image_handle next = current->next_image;
+				--dataset->imagecount;
+				free(current->imagedata);
+				free(current);
+				return next;
+                	}
+       		} 
+	}		
+}
+
+bool mnist_save(const_mnist_dataset_handle h, const char * filename)
+{
+	
+}
+
+
+
+
+
