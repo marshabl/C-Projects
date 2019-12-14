0526c92bb5c2afb793f940042cbdc21841a2f233
Blair Lewis Marshall <marshabl12@linux2.cs.uchicago.edu>
2019-12-14 15:58:08 -0600

    Add varstring

---
+++ final/varstring.h
@@ -0,0 +1,15 @@
+
+
+char * mystrdup (const char * s);
+
+char * newArray();
+
+char * append_char(char * str, char c);
+
+char ** add_word(char ** array, unsigned int * wordcount, const char * word_to_add);
+
+const char ** copy_array(char ** oldarray, unsigned int wordcount);
+
+void free_word_array(char ** array, unsigned int wordcount);
+
+
